dbc8cbffed51b4f53006de7a19eef6729bbd80c7
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-20 16:15:59 +0800

    update: Complete sidebar elements needed including icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,26 +1,54 @@
 import Image from "next/image";
+import styles from "../styles/Sidebar.module.css";
+
+import Head from "next/head";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+import {
+	faLinkedin,
+	faGithub,
+	faGitlab,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SideBar = () => {
 	return (
-		<div>
-			<div>
-				<Image
-					className="my-img"
-					src={"/images/me4.jpg"}
-					width={95}
-					height={90}
-				/>
-				<h3>
-					<span>Alvin</span> Acosta
-				</h3>
-				<p>Full Stack Web Developer</p>
-				{/* download resume */}
-				<p>Download Resume</p>
+		<>
+			<Head>
+				<style>{dom.css()}</style>
+			</Head>
+			<div className="sidebar-container">
+				<div className="sidebar_top">
+					<Image
+						className={styles.img}
+						src={"/images/me4.jpg"}
+						width={95}
+						height={90}
+					/>
+					<h3>
+						<span>Alvin</span> Acosta
+					</h3>
+					<p>Full Stack Web Developer</p>
+					{/* download resume */}
+					<p>Download Resume</p>
+				</div>
+				<div className={styles.socialIcons}>
+					<FontAwesomeIcon className={styles.icon} icon={faGithub} />
+					<FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+					<FontAwesomeIcon className={styles.icon} icon={faGitlab} />
+				</div>
+				<div className={styles.contactSection}>
+					<p>Manila, Philippines</p>
+					<p>alvinfloresacosta@gmail.com</p>
+					<p>0916-420-9977</p>
+				</div>
+				<div className="sidebar-buttons">
+					<p>Get in touch!</p>
+					<p>Switch Mode</p>
+				</div>
 			</div>
-			<div>{/* social icons */}</div>
-			<div>{/* contact info */}</div>
-			<div>{/* buttons  */}</div>
-		</div>
+		</>
 	);
 };
 
