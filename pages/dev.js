e8d97ea771f5410da79efdb7bc876018ed045a6b
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-25 15:37:14 +0800

    Start dev page, initial dev page top part

--- pages/dev.js
+++ pages/dev.js
@@ -1,30 +1,51 @@
 import styles from "../styles/Dev.module.css";
+import { FaFileDownload, FaPaperPlane, FaLinkedinIn } from "react-icons/fa";
 
 const dev = () => {
 	return (
 		<div className={styles.devContainer}>
 			<div className={styles.devTop}>
-				<div className="educ">
+				<div className={styles.education}>
 					<h4>Education</h4>
 					<div className="schools">
 						<div className="zuitt">
 							<h5>Zuitt Coding BootCamp Ph (2021)</h5>
-							<p>Full Stack Web Development</p>
+							<p>• Full Stack Web Development</p>
 						</div>
 						<div className="ama">
 							<h5>AMA-CU (2013-2017)</h5>
-							<p>B.S. Information Technology - Under graduate</p>
+							<p>• B.S. Information Technology - Under graduate</p>
 						</div>
 						<div className="feu">
 							<h5>Far Eastern University - NRMF (2007-2011)</h5>
-							<p>B.S. Nursing - Registered Nurse</p>
+							<p>• B.S. Nursing - Registered Nurse</p>
 						</div>
 					</div>
 				</div>
-				<div className="exp">
-					<h4>Experience</h4>
-					<p>RESUME</p>
-					<p>CONTACT ME</p>
+				<div className={styles.experience}>
+					<h4>Experience / Affiliations</h4>
+					<div className={styles.experienceBottom}>
+						<div className={styles.ctaDiv}>
+							<p>
+								<FaLinkedinIn className={styles.cta} />
+								LinkedIn
+							</p>
+							<p>
+								<FaFileDownload className={styles.cta} />
+								Resume
+							</p>
+							<p>
+								<FaPaperPlane className={styles.cta} />
+								Contact Me
+							</p>
+						</div>
+						<div className={styles.listDiv}>
+							<h6>• Cauld & Clark IT Solutions</h6>
+							<h6>• FreeCodeCamp Manila</h6>
+							<h6>• ReactJs Philippines</h6>
+							<h6>• Zuitt Coding Bootcamp</h6>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="language and tools">
