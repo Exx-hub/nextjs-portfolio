15ba23ed515c1bb74b86f61c65ef0172ceefa6c2
Exx-hub <alvinfloresacosta@gmail.com>
2021-06-08 20:48:37 +0800

    Update resume

--- pages/dev.js
+++ pages/dev.js
@@ -53,7 +53,7 @@ const dev = () => {
 									<FaLinkedinIn className={styles.cta} />
 									LinkedIn
 								</a>
-								<a href="/resume-alvinacosta.pdf" target="_blank" download>
+								<a href="/resume-alvin-acosta.pdf" target="_blank" download>
 									<FaFileDownload className={styles.cta} />
 									Resume
 								</a>
