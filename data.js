a751cc5cf978507cccc2cff5014a4d9d6696bf81
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-03-21 14:56:47 +0800

    change cmd website url

--- data.js
+++ data.js
@@ -5,225 +5,225 @@ import { MdDeveloperMode } from "react-icons/md";
 import { BsBook } from "react-icons/bs";
 
 export const skills = [
-	{
-		Icon: RiComputerLine,
-		title: "Frontend Development",
-		description:
-			"Create scalable websites using HTML, CSS and ReactJs and NextJs.",
-	},
-	{
-		Icon: FaServer,
-		title: "Backend Development",
-		description:
-			"Handle database, server and APIs with ExpressJs & other frameworks.",
-	},
-	{
-		Icon: AiOutlineApi,
-		title: "APIs and Microservices",
-		description:
-			"Develop clean and robust REST API's and microservices with NodeJs.",
-	},
-	{
-		Icon: AiOutlineAntDesign,
-		title: "Responsive Web Design",
-		description:
-			"Create clean, fully responsive website designs CSS and frameworks.",
-	},
-	{
-		Icon: MdDeveloperMode,
-		title: "Competetive Coder",
-		description:
-			"Solve problems daily on Hackerrank, Codewars and Freecodecamp.",
-	},
-	{
-		Icon: BsBook,
-		title: "Continuous Learning",
-		description:
-			"Daily progress on platforms like Udemy, YouTube, Freecodecamp.",
-	},
+  {
+    Icon: RiComputerLine,
+    title: "Frontend Development",
+    description:
+      "Create scalable websites using HTML, CSS and ReactJs and NextJs.",
+  },
+  {
+    Icon: FaServer,
+    title: "Backend Development",
+    description:
+      "Handle database, server and APIs with ExpressJs & other frameworks.",
+  },
+  {
+    Icon: AiOutlineApi,
+    title: "APIs and Microservices",
+    description:
+      "Develop clean and robust REST API's and microservices with NodeJs.",
+  },
+  {
+    Icon: AiOutlineAntDesign,
+    title: "Responsive Web Design",
+    description:
+      "Create clean, fully responsive website designs CSS and frameworks.",
+  },
+  {
+    Icon: MdDeveloperMode,
+    title: "Competetive Coder",
+    description:
+      "Solve problems daily on Hackerrank, Codewars and Freecodecamp.",
+  },
+  {
+    Icon: BsBook,
+    title: "Continuous Learning",
+    description:
+      "Daily progress on platforms like Udemy, YouTube, Freecodecamp.",
+  },
 ];
 
 export const projects = [
-	{
-		image: "/projectImages/cmd.png",
-		title: "CMD Pioneer Construction",
-		live: "https://cmdpioneerccph.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/cmdpioneerph",
-	},
-	{
-		image: "/projectImages/srf.png",
-		title: "SRF MNL Surf School NextJs",
-		live: "https://srf-mnl-react.herokuapp.com/",
-		srcCode: "https://github.com/Exx-hub/srf-mnl-react-version",
-	},
-	{
-		image: "/projectImages/invictus.png",
-		title: "Invictus Food Products Corp.",
-		live: "https://invictus-fpc.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/invictusfoodcorp",
-	},
-	{
-		image: "/projectImages/personal.png",
-		title: "Portfolio Website 1.0",
-		live: "https://exx-hub.gitlab.io/capstone-project-1/",
-		srcCode: "https://gitlab.com/Exx-hub/capstone-project-1",
-	},
-	{
-		image: "/projectImages/news.png",
-		title: "News App NextJs",
-		live: "https://newsapp-nextjs.vercel.app/",
-		srcCode: "https://github.com/Exx-hub/newsapp-nextjs",
-	},
-	{
-		image: "/projectImages/vanillasrf.png",
-		title: "MERN Srf Manila",
-		live: "https://csp3-client-tan.vercel.app/",
-		srcCode: "https://gitlab.com/tuitt/students/batch95/alvin-acosta/csp2",
-	},
-	{
-		image: "/projectImages/weather.png",
-		title: "React Weather App",
-		live: "https://weathertwopointo.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/weather2pointOW",
-	},
-	// {
-	// 	image: "/projectImages/reuxcart.png",
-	// 	title: "Asset Mgt System",
-	// 	live: "https://csp3-client-tan.vercel.app/",
-	// 	srcCode: "https://gitlab.com/Exx-hub/csp3-client",
-	// },
-	{
-		image: "/projectImages/reduxcart.png",
-		title: "Redux-Cart E-Commerce",
-		live: "https://reduxecommerce.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/redux-shopping-cart",
-	},
-	{
-		image: "/projectImages/meme.png",
-		title: "React Meme Generator",
-		live: "https://memegeneratorrr.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/memegenerator",
-	},
-	{
-		image: "/projectImages/firebase-gram.png",
-		title: "Firebase-Gram",
-		live: "https://firegrammm.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/firegram",
-	},
-	{
-		image: "/projectImages/jagi.png",
-		title: "Daily Quote Generator",
-		live: "https://binnieandalline.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/hundred-days-jagiya",
-	},
-	{
-		image: "/projectImages/drum.png",
-		title: "React Drum Machine",
-		live: "https://thea-unicorn-drum-machine.netlify.app/",
-		srcCode: "https://github.com/Exx-hub/react-drum-machine",
-	},
+  {
+    image: "/projectImages/cmd.png",
+    title: "CMD Pioneer Construction",
+    live: "https://cmdcorpph.com/",
+    srcCode: "https://github.com/Exx-hub/cmdpioneerph",
+  },
+  {
+    image: "/projectImages/srf.png",
+    title: "SRF MNL Surf School NextJs",
+    live: "https://srf-mnl-react.herokuapp.com/",
+    srcCode: "https://github.com/Exx-hub/srf-mnl-react-version",
+  },
+  {
+    image: "/projectImages/invictus.png",
+    title: "Invictus Food Products Corp.",
+    live: "https://invictus-fpc.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/invictusfoodcorp",
+  },
+  {
+    image: "/projectImages/personal.png",
+    title: "Portfolio Website 1.0",
+    live: "https://exx-hub.gitlab.io/capstone-project-1/",
+    srcCode: "https://gitlab.com/Exx-hub/capstone-project-1",
+  },
+  {
+    image: "/projectImages/news.png",
+    title: "News App NextJs",
+    live: "https://newsapp-nextjs.vercel.app/",
+    srcCode: "https://github.com/Exx-hub/newsapp-nextjs",
+  },
+  {
+    image: "/projectImages/vanillasrf.png",
+    title: "MERN Srf Manila",
+    live: "https://csp3-client-tan.vercel.app/",
+    srcCode: "https://gitlab.com/tuitt/students/batch95/alvin-acosta/csp2",
+  },
+  {
+    image: "/projectImages/weather.png",
+    title: "React Weather App",
+    live: "https://weathertwopointo.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/weather2pointOW",
+  },
+  // {
+  // 	image: "/projectImages/reuxcart.png",
+  // 	title: "Asset Mgt System",
+  // 	live: "https://csp3-client-tan.vercel.app/",
+  // 	srcCode: "https://gitlab.com/Exx-hub/csp3-client",
+  // },
+  {
+    image: "/projectImages/reduxcart.png",
+    title: "Redux-Cart E-Commerce",
+    live: "https://reduxecommerce.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/redux-shopping-cart",
+  },
+  {
+    image: "/projectImages/meme.png",
+    title: "React Meme Generator",
+    live: "https://memegeneratorrr.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/memegenerator",
+  },
+  {
+    image: "/projectImages/firebase-gram.png",
+    title: "Firebase-Gram",
+    live: "https://firegrammm.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/firegram",
+  },
+  {
+    image: "/projectImages/jagi.png",
+    title: "Daily Quote Generator",
+    live: "https://binnieandalline.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/hundred-days-jagiya",
+  },
+  {
+    image: "/projectImages/drum.png",
+    title: "React Drum Machine",
+    live: "https://thea-unicorn-drum-machine.netlify.app/",
+    srcCode: "https://github.com/Exx-hub/react-drum-machine",
+  },
 ];
 
 export const languages = [
-	{
-		title: "HTML",
-		image: "/langImages/html.png",
-	},
-	{
-		title: "CSS",
-		image: "/langImages/css.png",
-	},
-	{
-		title: "JavaScript",
-		image: "/langImages/js.png",
-	},
-	{
-		title: "ReactJs",
-		image: "/langImages/react.png",
-	},
-	{
-		title: "NextJs",
-		image: "/langImages/next.png",
-	},
-	{
-		title: "NodeJs",
-		image: "/langImages/node.png",
-	},
-	{
-		title: "MongoDb",
-		image: "/langImages/mongo.png",
-	},
-	{
-		title: "ExpressJs",
-		image: "/langImages/express.png",
-	},
-	{
-		title: "Redux",
-		image: "/langImages/redux.png",
-	},
-	{
-		title: "Bootstrap",
-		image: "/langImages/bootstrap.png",
-	},
-	{
-		title: "Sass",
-		image: "/langImages/sass.png",
-	},
-	{
-		title: "Material-Ui",
-		image: "/langImages/matui.png",
-	},
+  {
+    title: "HTML",
+    image: "/langImages/html.png",
+  },
+  {
+    title: "CSS",
+    image: "/langImages/css.png",
+  },
+  {
+    title: "JavaScript",
+    image: "/langImages/js.png",
+  },
+  {
+    title: "ReactJs",
+    image: "/langImages/react.png",
+  },
+  {
+    title: "NextJs",
+    image: "/langImages/next.png",
+  },
+  {
+    title: "NodeJs",
+    image: "/langImages/node.png",
+  },
+  {
+    title: "MongoDb",
+    image: "/langImages/mongo.png",
+  },
+  {
+    title: "ExpressJs",
+    image: "/langImages/express.png",
+  },
+  {
+    title: "Redux",
+    image: "/langImages/redux.png",
+  },
+  {
+    title: "Bootstrap",
+    image: "/langImages/bootstrap.png",
+  },
+  {
+    title: "Sass",
+    image: "/langImages/sass.png",
+  },
+  {
+    title: "Material-Ui",
+    image: "/langImages/matui.png",
+  },
 ];
 
 export const tools = [
-	{
-		title: "Git",
-		image: "/langImages/git.png",
-	},
-	{
-		title: "VsCode",
-		image: "/langImages/vscode.png",
-	},
-	{
-		title: "Dev Tools",
-		image: "/langImages/devtools.png",
-	},
-	{
-		title: "Figma",
-		image: "/langImages/figma.png",
-	},
-	{
-		title: "Canva",
-		image: "/langImages/canva.png",
-	},
+  {
+    title: "Git",
+    image: "/langImages/git.png",
+  },
+  {
+    title: "VsCode",
+    image: "/langImages/vscode.png",
+  },
+  {
+    title: "Dev Tools",
+    image: "/langImages/devtools.png",
+  },
+  {
+    title: "Figma",
+    image: "/langImages/figma.png",
+  },
+  {
+    title: "Canva",
+    image: "/langImages/canva.png",
+  },
 
-	{
-		title: "Git Bash",
-		image: "/langImages/bash.png",
-	},
-	{
-		title: "CMD",
-		image: "/langImages/cmd.png",
-	},
+  {
+    title: "Git Bash",
+    image: "/langImages/bash.png",
+  },
+  {
+    title: "CMD",
+    image: "/langImages/cmd.png",
+  },
 
-	{
-		title: "Postman",
-		image: "/langImages/post.png",
-	},
-	{
-		title: "Github",
-		image: "/langImages/hub.png",
-	},
-	{
-		title: "Gitlab",
-		image: "/langImages/lab.png",
-	},
-	{
-		title: "Heroku",
-		image: "/langImages/heroku.png",
-	},
-	{
-		title: "Netlify",
-		image: "/langImages/netlify.png",
-	},
+  {
+    title: "Postman",
+    image: "/langImages/post.png",
+  },
+  {
+    title: "Github",
+    image: "/langImages/hub.png",
+  },
+  {
+    title: "Gitlab",
+    image: "/langImages/lab.png",
+  },
+  {
+    title: "Heroku",
+    image: "/langImages/heroku.png",
+  },
+  {
+    title: "Netlify",
+    image: "/langImages/netlify.png",
+  },
 ];
